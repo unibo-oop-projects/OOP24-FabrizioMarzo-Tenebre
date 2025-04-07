17d7501bbb322abff3b4d070ab86d15c562ee916
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-04-07 14:27:22 +0200

    On build.gradle.kts put dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
 plugins{
     java
 }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.commons:commons-lang3:3.12.0") // Dependencies for Apache library
+}
