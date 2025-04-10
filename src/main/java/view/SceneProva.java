be4e213ea71017f860b851f92613b30465145cde
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-04-10 20:11:48 +0200

    Create the firs Panel for the SceneProva

--- src/main/java/view/SceneProva.java
+++ src/main/java/view/SceneProva.java
@@ -1,5 +1,33 @@
 package view;
 
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 public class SceneProva {
-    
+    private JFrame frame;
+    private SceneProvaPanel panel;
+
+    public SceneProva(final int w, final int h){
+        frame = new JFrame("L'armata delle Tenebre");
+        frame.setSize(w,h);
+        frame.setMinimumSize(new Dimension(w,h));
+        frame.setResizable(false);
+        panel = new SceneProvaPanel();
+        frame.getContentPane().add(panel);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void render(){
+        try {
+            SwingUtilities.invokeAndWait(()->{
+                frame.repaint();
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
---
+++ src/main/java/view/SceneProvaPanel.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.JPanel;
+
+public class SceneProvaPanel extends JPanel {
+    
+
+    
+    public SceneProvaPanel(final int w, final int h){
+        setSize(w,h);
+    }
+}
