d10a086e931c2719cafde01652cb710b8f0336a8
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-04-11 16:00:30 +0200

    Some Changes for try FactorySurvivorGame on ScenePanel

--- src/main/java/view/SceneProvaPanel.java
+++ src/main/java/view/SceneProvaPanel.java
@@ -18,7 +18,8 @@ public class SceneProvaPanel extends JPanel {
     private int centerY;
     
     // After i want SurvivorGameFactory.....
-    private SurvivorFactory surF = new SurvivorFactory();
+    //private SurvivorFactory surF = new SurvivorFactory();
+    private FactorySurvivorGame facSur = new FactorySurvivorGame();
 
     public SceneProvaPanel(final int w, final int h){
         setSize(w,h);
@@ -27,16 +28,19 @@ public class SceneProvaPanel extends JPanel {
     }
 
     public void paintComponent(Graphics g){
-        Survivor su1 = surF.createCommon(1000,20, Pair.of(30.0,40.0),Pair.of(5.0,2.0));
+
+        IGameSurvivor gamSur = facSur.gameSurvivorCommon(1000,20, Pair.of(30.0,40.0),Pair.of(5.0,2.0));
+        
         Graphics2D g2d = (Graphics2D) g ;
 
-        Stroke base = new BasicStroke(4f);
-        int x = (int) Math.round(su1.getCurrentPos().getLeft());
-        int y = (int) Math.round(su1.getCurrentPos().getRight());
+        //Stroke base = new BasicStroke(4f);
+        //int x = (int) Math.round(gamSur.getSurvivor().getCurrentPos().getLeft());
+        //int y = (int) Math.round(gamSur.getSurvivor().getCurrentPos().getRight());
 
         // I want this on a Method named draw on SurvivorGame class
-        g2d.setColor(Color.BLUE);
-        g2d.setStroke(base);
-        g2d.drawOval(x,y, 40, 40);
+        //g2d.setColor(Color.BLUE);
+        //g2d.setStroke(base);
+        //g2d.drawOval(x,y, 40, 40);
+        gamSur.updateGraphics(g2d);
     }
 }
