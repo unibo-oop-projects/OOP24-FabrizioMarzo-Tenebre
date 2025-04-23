9e61b1d06edad122be2e74bfa7e94c03ed00fd92
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-04-23 02:21:51 +0200

    Writting the javadoc: Level(Interface) and TutorialLevel(Class)

--- src/main/java/model/level/Level.java
+++ src/main/java/model/level/Level.java
@@ -2,10 +2,29 @@ package model.level;
 
 import model.entities_game.survivor_game.IGameSurvivor;
 
+/**
+ * Interface representing a game level.
+ * <p>
+ * A level manages the game survivor and is responsible for updating the game state
+ * over time.
+ */
 public interface Level {
 
+    /**
+     * Returns the survivor entity associated with this level.
+     *
+     * @return the {@link IGameSurvivor} present in the level
+     */
     public IGameSurvivor getSurvivorOnLevel(); 
 
+    /**
+     * Updates the state of the level.
+     * <p>
+     * This method is typically called to update all entities
+     * within the level based on the time elapsed.
+     *
+     * @param dt the time delta in milliseconds since the last update
+     */
     public void updateState(final int dt);
     
 }
--- src/main/java/model/level/TutorialLevel.java
+++ src/main/java/model/level/TutorialLevel.java
@@ -5,20 +5,42 @@ import org.apache.commons.lang3.tuple.Pair;
 import model.entities_game.survivor_game.FactorySurvivorGame;
 import model.entities_game.survivor_game.IGameSurvivor;
 
+/**
+ * Represents the tutorial level of the game.
+ * <p>
+ * This level contains a single common survivor and initializes it with
+ * predefined attributes such as health, attack power, position, and velocity.
+ * It also handles updating the survivor's internal state over time.
+ */
 public class TutorialLevel implements Level {
     private FactorySurvivorGame fact = new FactorySurvivorGame(); 
     private IGameSurvivor survivorCommon;
 
-
+    /**
+     * Constructs the tutorial level with a preconfigured {@link IGameSurvivor}.
+     */
     public TutorialLevel(){
         this.survivorCommon = fact.gameSurvivorCommon(1000,20, Pair.of(390.0,40.0),Pair.of(50.0,0.0));
     }
 
+    /**
+     * {@inheritDoc}
+     * 
+     * @return the survivor entity present in this tutorial level
+     */
     public IGameSurvivor getSurvivorOnLevel(){
             return this.survivorCommon;
     }
 
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Updates the survivor's internal state based on the elapsed time.
+     *
+     * @param dt the time delta in milliseconds since the last update
+     */
+    @Override
     public void updateState(final int dt){
-        survivorCommon.getSurvivor().updateState(dt);
+        survivorCommon.getSurvivor().updateSurvivor(dt);
     }
 }
