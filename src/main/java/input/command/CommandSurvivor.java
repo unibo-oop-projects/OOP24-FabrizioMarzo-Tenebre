e408b56fafcd5de07cfeb29fc22faa2ac4875677
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-04-24 02:22:08 +0200

    Writting the javadoc: CommandSurvivor(Class)

--- src/main/java/input/command/CommandSurvivor.java
+++ src/main/java/input/command/CommandSurvivor.java
@@ -4,8 +4,22 @@ import java.util.function.Consumer;
 
 import model.entities.survivor.base.Survivor;
 
+/**
+ * Utility class to execute commands on a {@link Survivor} instance.
+ * 
+ * <p>This class provides a method to issue a command, represented as a
+ * {@link Consumer}, to a given Survivor object.</p>
+ */
 public class CommandSurvivor {
 
+    /**
+     * Issues a command to the specified {@link Survivor}.
+     *
+     * @param sur the {@link Survivor} instance on which the command will be executed
+     * @param command a {@link Consumer} representing the command to apply to the Survivor
+     * 
+     * <p>The command is executed by invoking {@code accept} on the provided Consumer.</p>
+     */
     public static void issue(final Survivor sur, Consumer<Survivor> command){
         command.accept(sur);
     }
