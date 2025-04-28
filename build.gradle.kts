18398a7cbab34d3148feacfa06bc6eed084d1c44
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-04-28 18:44:45 +0200

    Add on build.gradle.kts the plugin SpotBus

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins{
     java
     application
+    
+    id("org.danilopianini.gradle-java-qa") version "1.108.0"
 }
 
 repositories {
@@ -12,5 +14,8 @@ application {
 }
 
 dependencies {
+    // Suppressions for SpotBugs
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.7.3")
+
     implementation("org.apache.commons:commons-lang3:3.12.0") // Dependencies for Apache library
 }
