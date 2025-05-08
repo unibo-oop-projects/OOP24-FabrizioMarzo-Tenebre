af448cdaf1d77cd0de76f20ecdf591a2e157b549
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-05-08 21:11:37 +0200

    Modify position of methods on Interface

--- src/main/java/model/entities/survivor/Survivor.java
+++ src/main/java/model/entities/survivor/Survivor.java
@@ -15,74 +15,74 @@ public interface Survivor {
      *
      * @return The amount of damage inflicted by the attack
      */
-    public int attack();
+    int attack();
 
     /**
      * Applies damage to the survivor.
      *
      * @param dm The amount of damage taken
      */
-    public void damageSuffer(final int dm);
+    void damageSuffer(final int dm);
     
-    /**
-     * Returns the current position of the survivor.
-     *
-     * @return A pair (x, y) representing the current position
-     */
-    public Pair<Double,Double> getCurrentPos();
-
-    /**
-     * Returns the current velocity of the survivor.
-     *
-     * @return A pair (vx, vy) representing the current velocity
-     */
-    public Pair<Double,Double> getCurrentVel();
-
-    /**
-     * Returns the base velocity of the survivor.
-     *
-     * @return A pair (vx, vy) representing the base velocity
-     */    
-    public Pair<Double,Double> getBaseSurvivorVel();
-
-    /**
-     * Returns the current state of the survivor.
-     *
-     * @return The current state
-     */    
-    public SurvivorState getState();
-
-    /**
-     * Returns the number of remaining health of the survivor.
-     *
-     * @return The current health value
-     */
-    public int getLive();
+    void updatePhysics(final int dt);
+    
+    void updateBBox(final Pair<Double,Double> newPos);
 
     /**
      * Sets the survivor's velocity.
      *
      * @param vel A pair (vx, vy) representing the new velocity
      */
-    public void setVelocity(final Pair<Double,Double> vel);
+    void setVelocity(final Pair<Double,Double> vel);
 
-    public void setPosition(final Pair<Double, Double> pos);
+    void setPosition(final Pair<Double, Double> pos);
 
     /**
      * Sets a new state for the survivor.
      *
      * @param newState The new state to apply
      */
-    public void setState(final SurvivorState newState);
+    void setState(final SurvivorState newState);
 
-    public BoundingBox getBBox();
+    /**
+     * Returns the number of remaining health of the survivor.
+     *
+     * @return The current health value
+     */
+    int getLive();
+    
+    int getWidth();
 
-    public void updateBBox(final Pair<Double,Double> newPos);
+    int getHeight();
 
-    public  int getWidth();
+    /**
+     * Returns the current position of the survivor.
+     *
+     * @return A pair (x, y) representing the current position
+     */
+    Pair<Double,Double> getCurrentPos();
 
-    public  int getHeight();
+    /**
+     * Returns the current velocity of the survivor.
+     *
+     * @return A pair (vx, vy) representing the current velocity
+     */
+    Pair<Double,Double> getCurrentVel();
 
-    public void updatePhysics(final int dt);
+    /**
+     * Returns the base velocity of the survivor.
+     *
+     * @return A pair (vx, vy) representing the base velocity
+     */    
+    Pair<Double,Double> getBaseSurvivorVel();
+
+    /**
+     * Returns the current state of the survivor.
+     *
+     * @return The current state
+     */    
+    SurvivorState getState();
+
+    BoundingBox getBBox();
 
 }
