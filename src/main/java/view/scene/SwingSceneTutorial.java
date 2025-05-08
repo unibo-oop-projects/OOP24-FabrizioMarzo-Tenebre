c64053c195871d707998a2a3b20a66054c45dbb8
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-05-08 16:29:29 +0200

    Add Scale Factor for the panel

--- src/main/java/view/scene/SwingSceneTutorial.java
+++ src/main/java/view/scene/SwingSceneTutorial.java
@@ -1,5 +1,7 @@
 package view.scene;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -7,6 +9,8 @@ import java.awt.Graphics2D;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 import game.entities_game.FactorySurvivorGame;
 import game.entities_game.IGameSurvivor;
@@ -17,6 +21,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import input.input_controller.InputController;
+import model.bounding_box.BoundingBox;
 import model.level.Level;
 import view.graphics_component.GraphicsSurvivor;
 import view.graphics_component.SwingGraphicsSurvivor;
@@ -77,27 +82,61 @@ public class SwingSceneTutorial implements Scene {
     public class SceneTutorialPanel extends JPanel implements KeyListener {
 
         private int w,h;
+        private int levelw,levelh;
+        private int centerX,centerY;
+        private double ratioX,ratioY;
+        BoundingBox bbox = tutLevel.getLevelBBox();
+        int x0 = this.getXinPixel(bbox.getULcorner());
+        int y0 = this.getYinPixel(bbox.getULcorner());
+        int x1 = this.getXinPixel(bbox.getBRcorner());
+        int y1 = this.getYinPixel(bbox.getBRcorner());
 
         public SceneTutorialPanel(final int w, final int h){
-            this.w = w;
+
             this.h = h;
-            setPanelSize();
+            this.w = w;
+
+            System.out.println("Height panel " + h);
+            System.out.println("Width panel " + w);
+
+            this.levelh = (int) tutLevel.getTutorialLevelHeight();
+            this.levelw = (int) tutLevel.getTutorialLevelWidth();
+            
+            System.out.println("Height level " + levelh);
+            System.out.println("Width level " + levelw);
+
+            this.ratioX = (double) w / levelw;
+            this.ratioY = (double) h / levelh;
+
+            System.out.println("RatioX " + ratioX);
+            System.out.println("RatioiY" + ratioY);
+
+            centerX = w /2 ;
+            centerY = h /2 ;
+
             this.addKeyListener(this);
             setFocusable(true);
             setFocusTraversalKeysEnabled(false);
         }
 
-        public void setPanelSize(){
-            Dimension size = new Dimension(w,h);
-            setPreferredSize(size);
-        }
-
         public void paintComponent(Graphics g){
         
+            int x0 = this.getXinPixel(bbox.getULcorner());
+            int y0 = this.getYinPixel(bbox.getULcorner());
+            int x1 = this.getXinPixel(bbox.getBRcorner());
+            int y1 = this.getYinPixel(bbox.getBRcorner());
+    
+            System.out.println("Altezza panel scale  " + (x1-x0));
+            System.out.println("Grandezza panel scale " + Math.abs(y1-y0));
+
             Graphics2D g2d = (Graphics2D) g ;
             GraphicsSurvivor graphSur = new SwingGraphicsSurvivor(g2d,this.h);
-
             gamSur.updateGraphics(graphSur);
+
+            g2d.setColor(Color.black);
+            g2d.setStroke(new BasicStroke(30));
+            g2d.drawRect(0, 0, x1-x0, Math.abs(y1-y0));
+
         }
 
         @Override
@@ -112,6 +151,15 @@ public class SwingSceneTutorial implements Scene {
 
         @Override
         public void keyTyped(KeyEvent e) {}
+
+        private int getXinPixel(Pair<Double,Double> pos){
+            return (int) Math.round(centerX + pos.getLeft() * ratioX);
+        }
+
+        private int getYinPixel(Pair<Double,Double> pos){
+            return (int) Math.round(centerY - pos.getRight() * ratioY);
+        }
+ 
 
     }
 
