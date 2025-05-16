e03a0e58d72d03cef95efedaf8121a7293b56405
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-05-16 17:45:39 +0200

    Modify the Interface Zombie

--- src/main/java/model/entities/zombie/Zombie.java
+++ src/main/java/model/entities/zombie/Zombie.java
@@ -1,9 +1,35 @@
 package model.entities.zombie;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import model.bounding_box.BoundingBox;
+import model.entities.EntitieState;
+
 /**
  * Zombie interface representing a zombie in the game.
  * It defines the basic actions and state management for a zombie entity.
  */
 public interface Zombie {
     
+    int attack();
+
+    void damageSuffer(final int dm);
+
+    void updatePhysics(final int dt);
+
+    void setPosition(final Pair<Double, Double> pos);
+
+    void setState(final EntitieState newState);
+
+    int getWidth();
+
+    int getHeight();
+
+    Pair<Double,Double> getCurrentPos();
+
+    Pair<Double,Double> getCurrentVel();
+
+    EntitieState getState();
+
+    BoundingBox getBBox();
 }
