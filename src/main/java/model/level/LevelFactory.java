6c7a2523a6ae980280c10232bdb5515d00e67b1a
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-05-19 14:18:23 +0200

    On the Level Factory now,don't create the Survivor and the Zombie on the Level

--- src/main/java/model/level/LevelFactory.java
+++ src/main/java/model/level/LevelFactory.java
@@ -3,23 +3,17 @@ package model.level;
 import org.apache.commons.lang3.tuple.Pair;
 
 import model.bounding_box.RectBoundingBox;
-import model.entities.survivor.Survivor;
-import model.entities.survivor.SurvivorFactory;
 import model.physics.physics_level.PhysicsLevelTutComponent;
 
 public class LevelFactory {
     
-    private SurvivorFactory surFact = new SurvivorFactory();
-
     public Level createTutorialLevel(){
-        Survivor sur  = surFact.createCommonSurvivor(1000,20, Pair.of(1000.0,1000.0),Pair.of(200.0,0.0));
         final Double lvlWidth = 4000d;
         final Double lvlHeight = 2500d;
         return new TutorialLevel(lvlWidth,
                                 lvlHeight,
                                 new RectBoundingBox(Pair.of(0.0,lvlHeight), Pair.of(lvlWidth,0.0)),
-                                new PhysicsLevelTutComponent(),
-                                sur);
+                                new PhysicsLevelTutComponent());
     }
 
 }
