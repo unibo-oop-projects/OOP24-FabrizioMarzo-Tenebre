2e810a47b7707419ae4b33a70e0a79d9754885db
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-05-19 14:28:47 +0200

    Modify the Physics of the Clicker's Zombie, now Print only his  class object

--- src/main/java/model/physics/physics_entities/PhysicsClickerComponent.java
+++ src/main/java/model/physics/physics_entities/PhysicsClickerComponent.java
@@ -1,18 +1,11 @@
 package model.physics.physics_entities;
 
-import org.apache.commons.lang3.tuple.Pair;
-
 import model.entities.zombie.Zombie;
-import utils.PairUtils;
 
 public class PhysicsClickerComponent implements PhysicsZombieComponent{
 
     @Override
     public void updateZombie(Zombie zob, int dt) {
-        zob.setPosition(PairUtils.sum(zob.getCurrentPos(), nextPos(dt, zob.getCurrentVel())));
-    }
-    
-    private Pair<Double,Double> nextPos(final int dt, final Pair<Double,Double> vel){
-        return PairUtils.mul(vel,0.001*dt);
+        System.out.println(zob.getClass().getSimpleName() + " Zombie " + dt + "Time Pass");
     }
 }
