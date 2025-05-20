1bc9855a418ef387e0b94f911b544ad05fd6dca7
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-05-20 14:35:58 +0200

    Now the Zombie's velocity is create on the Zombie Factoty

--- src/main/java/model/entities/zombie/ZombieFactory.java
+++ src/main/java/model/entities/zombie/ZombieFactory.java
@@ -8,11 +8,12 @@ import model.physics.physics_entities.PhysicsClickerComponent;
 
 public class ZombieFactory {
 
-    public Zombie createClickerZombie(final Pair<Double,Double> pos, final Pair<Double,Double> vel){
+    public Zombie createClickerZombie(final Pair<Double,Double> pos){
         final int live = 1000;
         final int attack = 20;
         final int width = 90;
         final int height = 230;
+        final Pair<Double, Double> vel = Pair.of(150.0, 0.0);
         return new Clicker(live, attack, 
                             width, height, 
                             new MutablePair<>(pos.getLeft(),pos.getRight()),
