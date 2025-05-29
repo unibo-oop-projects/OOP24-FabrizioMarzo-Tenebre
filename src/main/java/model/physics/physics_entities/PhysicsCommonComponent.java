3d1aedd99f29c0bf3c61bdcd8c0851e4b1144283
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-05-30 01:31:21 +0200

    Add a controll if the Survivor is attacking

--- src/main/java/model/physics/physics_entities/PhysicsCommonComponent.java
+++ src/main/java/model/physics/physics_entities/PhysicsCommonComponent.java
@@ -2,6 +2,8 @@ package model.physics.physics_entities;
 
 import org.apache.commons.lang3.tuple.Pair;
 
+import model.armory.munition.Munition;
+import model.entities.EntitieState;
 import model.entities.survivor.Survivor;
 import utils.PairUtils;
 
@@ -10,9 +12,16 @@ public class PhysicsCommonComponent implements PhysicsSurvivorComponent {
     @Override
     public void updateSurvivor(final Survivor sur,final int dt) {
         sur.setPosition(PairUtils.sum(sur.getCurrentPos(),nextPos(dt,sur.getCurrentVel())));
-        System.out.println("For the Survivor" + PairUtils.toIntPair(sur.getBBox().getULcorner()) +
-                           " and " + PairUtils.toIntPair(sur.getBBox().getBRcorner()));
-        System.out.println("I am modifing my state with th PhsycsComponent");
+
+        // System.out.println("For the Survivor" + PairUtils.toIntPair(sur.getBBox().getULcorner()) +
+        //                    " and " + PairUtils.toIntPair(sur.getBBox().getBRcorner()));
+        // System.out.println("I am modifing my state with th PhsycsComponent");
+        
+        if (sur.getState() == EntitieState.ATTACK){
+            for (Munition m : sur.shoot(dt) ){
+                m.moveShoot(dt);
+            }
+        }
     }
     
     private Pair<Double,Double> nextPos(final int dt, final Pair<Double,Double> vel){
