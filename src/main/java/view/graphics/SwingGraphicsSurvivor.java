2f34dc285164c387990843f2df3ec5868840730a
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-05-30 01:29:46 +0200

    Delete some System.out.printl

--- src/main/java/view/graphics/SwingGraphicsSurvivor.java
+++ src/main/java/view/graphics/SwingGraphicsSurvivor.java
@@ -23,14 +23,8 @@ public class SwingGraphicsSurvivor implements GraphicsSurvivor{
         int scaleSurPosX = viewScale.getXinPixel(sur.getCurrentPos());
         int scaleSurPosY = viewScale.getViewHeight()-viewScale.getYinPixel(sur.getCurrentPos());
 
-        System.out.println("Not Scale Width " + sur.getWidth());
-        System.out.println("Not Scale Heigth " + sur.getHeight());
-
         int scaleSurWidth = (int) Math.round(sur.getWidth() * viewScale.getRatioX());
         int scaleSurHeight = (int) Math.round(sur.getHeight()* viewScale.getRatioY());
-
-        System.out.println("Scale Width " + scaleSurWidth);
-        System.out.println("Scale Heigth " + scaleSurHeight);
         
         // Draw the Survivor
         g2d.drawImage(image, scaleSurPosX, scaleSurPosY-scaleSurHeight, scaleSurWidth, scaleSurHeight, null);
