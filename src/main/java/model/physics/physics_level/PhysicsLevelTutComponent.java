bc2ea8b5ee770eb9f6027612c019eaac757cdb2a
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-05-30 22:37:55 +0200

    Now on PhysicsLevelTutComponent check if the Survivor is Attacking and shoot

--- src/main/java/model/physics/physics_level/PhysicsLevelTutComponent.java
+++ src/main/java/model/physics/physics_level/PhysicsLevelTutComponent.java
@@ -1,10 +1,15 @@
 package model.physics.physics_level;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang3.tuple.Pair;
 
 import model.ai.behavior.AINPCBehavior;
 import model.ai.behavior.FactoryAINPCBehavior;
+import model.armory.munition.Munition;
+import model.entities.EntitieState;
 import model.entities.survivor.Survivor;
 import model.entities.zombie.Zombie;
 import model.level.Level;
@@ -13,9 +18,18 @@ public class PhysicsLevelTutComponent implements PhysicsLevelComponent {
 
     FactoryAINPCBehavior factAINPC = new FactoryAINPCBehavior();
     AINPCBehavior<Survivor,Zombie> baseAIZombie = factAINPC.createBaseNPCBehavior();
+    private List<Munition> activeMunitions = new ArrayList<>();
+    private List<Munition> newMunitions = new ArrayList<>();
 
     @Override
     public void updateLevel(final Level lv,final int dt) {
+
+        this.checkSurvivorShoot(lv.getSurvivorOnLevel(), dt);
+        
+        System.out.println("Number of Munitions on level: " + this.activeMunitions.size());
+        
+        this.updateMunitions(dt);
+
         lv.getSurvivorOnLevel().updatePhysics(dt);
         lv.getZombieOnLevel().stream()
                              .forEach(zombie -> {
@@ -61,4 +75,27 @@ public class PhysicsLevelTutComponent implements PhysicsLevelComponent {
         lv.getSurvivorOnLevel().setVelocity(Pair.of(0.0, 0.0));
     }
   
+
+    private void checkSurvivorShoot(final Survivor sur, final int dt){
+        if (sur.getState() == EntitieState.ATTACK) {
+            System.out.println("The Survivor is Shooting");
+            this.newMunitions.addAll(sur.getWeapon().shoot(dt));
+            System.out.println("!!!!!!!!!!!!!!!!!!" + this.newMunitions);
+            if (!newMunitions.isEmpty()) {
+                this.activeMunitions.addAll(newMunitions);
+                System.out.println("New Munitions are: " + newMunitions.size());
+            }
+            this.newMunitions.clear();
+        }
+    }
+
+    private void updateMunitions(final int dt) {
+        this.activeMunitions.stream()
+                            .forEach(munition -> {
+                                munition.moveShoot(dt);
+                                System.out.println(munition.getBBox().getULcorner());
+                            });
+    }
+
+
 }
