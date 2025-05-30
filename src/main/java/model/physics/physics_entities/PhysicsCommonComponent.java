0c3ca519e78236349d8f398b205e46f491e59e03
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-05-30 22:49:21 +0200

    Delete Comments

--- src/main/java/model/physics/physics_entities/PhysicsCommonComponent.java
+++ src/main/java/model/physics/physics_entities/PhysicsCommonComponent.java
@@ -9,9 +9,6 @@ public class PhysicsCommonComponent implements PhysicsSurvivorComponent {
     @Override
     public void updateSurvivor(final Survivor sur,final int dt) {
         sur.setPosition(PairUtils.sum(sur.getCurrentPos(),nextPos(dt,sur.getCurrentVel())));
-        System.out.println("For the Survivor" + PairUtils.toIntPair(sur.getBBox().getULcorner()) +
-                           " and " + PairUtils.toIntPair(sur.getBBox().getBRcorner()));
-        System.out.println("I am modifing my state with th PhsycsComponent");
     }
     
     private Pair<Double,Double> nextPos(final int dt, final Pair<Double,Double> vel){
