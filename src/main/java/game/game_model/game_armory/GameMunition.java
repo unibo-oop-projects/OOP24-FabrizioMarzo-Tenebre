ed78ea2463dafcf1ed5afabf0f8d7e95e0b67fec
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-06-03 14:08:44 +0200

    Now the method updateGraphics take on input the GraphicsMunition

--- src/main/java/game/game_model/game_armory/GameMunition.java
+++ src/main/java/game/game_model/game_armory/GameMunition.java
@@ -1,13 +1,17 @@
 package game.game_model.game_armory;
 
 import model.armory.munition.Munition;
+import view.graphics.GraphicsMunition;
+import view.graphics_component.armory.GraphicsMunitionComponent;
 
 public class GameMunition implements IGameMunition{
 
     private Munition mun;
+    private GraphicsMunitionComponent imgM;
 
-    public GameMunition(final Munition mun){
+    public GameMunition(final Munition mun, final GraphicsMunitionComponent imgM){
         this.mun = mun;
+        this.imgM = imgM;
     }
 
 
@@ -17,9 +21,8 @@ public class GameMunition implements IGameMunition{
     }
 
     @Override
-    public void updateGraphics() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateGraphics'");
+    public void updateGraphics(final GraphicsMunition graphicsMun) {
+        imgM.update(this.getMunition(), graphicsMun);
     }
     
 }
--- src/main/java/game/game_model/game_armory/IGameMunition.java
+++ src/main/java/game/game_model/game_armory/IGameMunition.java
@@ -1,12 +1,12 @@
 package game.game_model.game_armory;
 
 import model.armory.munition.Munition;
+import view.graphics.GraphicsMunition;
 
 public interface IGameMunition {
     
     Munition getMunition();
 
-    void updateGraphics();
-
+    void updateGraphics(final GraphicsMunition graphicsMun);
 
 }
