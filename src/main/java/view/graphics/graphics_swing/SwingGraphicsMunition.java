4fbc100b9a07851cad6d1b0091a296b146f48ca5
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-06-03 16:09:47 +0200

    The class SwingGraphicsMunition draw the munition by Swing

--- src/main/java/view/graphics/graphics_swing/SwingGraphicsMunition.java
+++ src/main/java/view/graphics/graphics_swing/SwingGraphicsMunition.java
@@ -7,26 +7,28 @@ import model.armory.munition.Munition;
 import view.graphics.GraphicsMunition;
 import view.graphics_util.IViewScale;
 
-public class SwingGraphicsMuition implements GraphicsMunition{
+public class SwingGraphicsMunition implements GraphicsMunition{
 
     private Graphics2D g2d;
     private IViewScale viewScale;
 
-    public SwingGraphicsMuition(final Graphics2D g2d, final IViewScale viewScale){
+    public SwingGraphicsMunition(final Graphics2D g2d, final IViewScale viewScale){
         this.g2d = g2d;
         this.viewScale = viewScale;
     }
 
     @Override
-    public void drawMunition(Munition mun, BufferedImage image) {
+    public void drawMunition(final Munition mun,final BufferedImage image) {
+
+        System.out.println("I'M PAITTING THE MUNITION ");
 
         int scaleSurPosX = viewScale.getXinPixel(mun.getCurrentPos());
         int scaleSurPosY = viewScale.getViewHeight()-viewScale.getYinPixel(mun.getCurrentPos());
 
-        int scaleSurWidth = (int) Math.round(mun.getWidth() * viewScale.getRatioX());
+       // int scaleSurWidth = (int) Math.round(mun.getWidth() * viewScale.getRatioX());
         int scaleSurHeight = (int) Math.round(mun.getWidth()* viewScale.getRatioY());
         
-        g2d.drawImage(image, scaleSurPosX, scaleSurPosY-scaleSurHeight, scaleSurWidth, scaleSurHeight, null);
+        g2d.drawImage(image, scaleSurPosX, scaleSurPosY-scaleSurHeight, 10, 10, null);
 
     }
     
