15f50ce141167e0e71acdc5f99f815965b6a82a5
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-06-05 22:49:16 +0200

    Create a ZombieState enum

---
+++ src/main/java/model/entities/zombie/ZombieState.java
@@ -0,0 +1,32 @@
+package model.entities.zombie;
+
+
+public enum ZombieState {
+     
+    ZOMBIE_IDLE(0),
+
+    ZOMBIE_MOVE_LEFT(1),
+
+    ZOMBIE_MOVE_RIGHT(2),
+
+    ZOMBIE_MOVE_UP(3),
+
+    ZOMBIE_MOVE_DOWN(4),
+
+    ZOMBIE_ATTACK(5),
+
+    ZOMBIE_DAMAGE(6),
+
+    ZOMBIE_DEAD(7);
+
+    private final int index;
+
+    ZombieState(final int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+}
