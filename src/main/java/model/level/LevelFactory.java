c26048edfc57f49c11fa2badb931d2bd01165ca3
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-06-05 18:31:05 +0200

    Delete Magic Numbers

--- src/main/java/model/level/LevelFactory.java
+++ src/main/java/model/level/LevelFactory.java
@@ -7,12 +7,15 @@ import model.physics.physics_level.PhysicsLevelTutComponent;
 
 public class LevelFactory {
     
+    private static final double LEVEL_MIN_X = 0.0;
+    private static final double LEVEL_MIN_Y = 0.0;
+
     public Level createTutorialLevel(){
         final Double lvlWidth = 4000d;
         final Double lvlHeight = 2500d;
         return new TutorialLevel(lvlWidth,
                                 lvlHeight,
-                                new RectBoundingBox(Pair.of(0.0,lvlHeight), Pair.of(lvlWidth,0.0)),
+                                new RectBoundingBox(Pair.of(LEVEL_MIN_X,lvlHeight), Pair.of(lvlWidth,LEVEL_MIN_Y)),
                                 new PhysicsLevelTutComponent());
     }
 
