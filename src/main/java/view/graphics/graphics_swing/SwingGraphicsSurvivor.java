9abe1f910eb6000154e05871a775d98a327551e6
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-06-09 14:31:26 +0200

    create two private methods drawSurvior or Zombie Health and the private method drawBoundingBox

--- src/main/java/view/graphics/graphics_swing/SwingGraphicsSurvivor.java
+++ src/main/java/view/graphics/graphics_swing/SwingGraphicsSurvivor.java
@@ -1,5 +1,6 @@
 package view.graphics.graphics_swing;
 
+import java.awt.Color;
 //import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
@@ -29,14 +30,50 @@ public class SwingGraphicsSurvivor implements GraphicsSurvivor{
         
         // Draw the Survivor
         g2d.drawImage(image, scaleSurPosX, scaleSurPosY-scaleSurHeight, scaleSurWidth, scaleSurHeight, null);
-            
-        // int scaleBboxUx = viewScale.getXinPixel(sur.getBBox().getULcorner());
-        // int scaleBboxUy = viewScale.getViewHeight() - viewScale.getYinPixel(sur.getBBox().getULcorner());
-
-        // // Draw the Survivor BoundingBox
-        // g2d.setColor(Color.blue);
-        // g2d.drawRect(scaleBboxUx, scaleBboxUy,scaleSurWidth,scaleSurHeight);
-
+        drawSurvivorHealth(sur, scaleSurPosX, scaleSurPosY, scaleSurHeight, scaleSurWidth);
     }
 
+    private void drawSurvivorHealth(final Survivor sur,final int scaleSurPosX, final int scaleSurPosY,
+                                        final int scaleSurHeight, final int scaleSurWidth){
+
+        // Dimension of the rectangle (life bar)
+        int rectWidth = scaleSurWidth;
+        int rectHeight = 6;  // height rect
+        
+        // Position of the rectangle 
+        int rectX = scaleSurPosX;
+        int rectY = scaleSurPosY - scaleSurHeight - rectHeight - 5;  // 5 pixel up zombie head 
+        
+        // Calculate actual width according to Survivor life
+        final int maxLife = sur.getMaxSurvivorHealth(); 
+        int currentLife = sur.getLive();
+        int currentWidth = (int) ((double) currentLife / maxLife * rectWidth);
+
+        // Draw background (black) rectangle
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect(rectX, rectY, rectWidth, rectHeight);
+
+        // Decide life bar color based on health ratio
+        float healthRatio = (float) currentLife / maxLife;
+        if (healthRatio > 0.5) {
+            g2d.setColor(Color.GREEN);
+        } else if (healthRatio > 0.25) {
+            g2d.setColor(Color.YELLOW);
+        } else {
+            g2d.setColor(Color.RED);
+        }
+
+        // Draw life bar
+        g2d.fillRect(rectX, rectY, currentWidth, rectHeight);
+    }
+
+    @SuppressWarnings("unused")
+    private void drawBoundingBox(final Survivor sur, final int scaleSurHeight, final int scaleSurWidth){
+        int scaleBboxUx = viewScale.getXinPixel(sur.getBBox().getULcorner());
+        int scaleBboxUy = viewScale.getViewHeight() - viewScale.getYinPixel(sur.getBBox().getULcorner());
+    
+        // Draw the Survivor BoundingBox
+        g2d.setColor(Color.blue);
+        g2d.drawRect(scaleBboxUx, scaleBboxUy,scaleSurWidth,scaleSurHeight);
+    }
 }
--- src/main/java/view/graphics/graphics_swing/SwingGraphicsZombie.java
+++ src/main/java/view/graphics/graphics_swing/SwingGraphicsZombie.java
@@ -1,7 +1,7 @@
 package view.graphics.graphics_swing;
 
 import java.awt.image.BufferedImage;
-//import java.awt.Color;
+import java.awt.Color;
 import java.awt.Graphics2D;
 
 import model.entities.zombie.Zombie;
@@ -19,7 +19,7 @@ public class SwingGraphicsZombie implements GraphicsZombie {
     }
 
     @Override
-    public void drawZombie(Zombie zob, BufferedImage image) {
+    public void drawZombie(final Zombie zob,final BufferedImage image) {
         
         int scaleSurPosX = viewScale.getXinPixel(zob.getCurrentPos());
         int scaleSurPosY = viewScale.getViewHeight()-viewScale.getYinPixel(zob.getCurrentPos());
@@ -28,14 +28,48 @@ public class SwingGraphicsZombie implements GraphicsZombie {
         int scaleSurHeight = (int) Math.round(zob.getHeight()* viewScale.getRatioY());
 
         g2d.drawImage(image, scaleSurPosX, scaleSurPosY-scaleSurHeight, scaleSurWidth, scaleSurHeight, null);
-
-        // int scaleBboxUx = viewScale.getXinPixel(zob.getBBox().getULcorner());
-        // int scaleBboxUy = viewScale.getViewHeight() - viewScale.getYinPixel(zob.getBBox().getULcorner());
-
-        // // Draw the Survivor BoundingBox
-        // g2d.setColor(Color.blue);
-        // g2d.drawRect(scaleBboxUx, scaleBboxUy,scaleSurWidth,scaleSurHeight);
+        drawZombieHealth(zob, scaleSurPosX, scaleSurPosY, scaleSurHeight, scaleSurWidth);
+        //drawBoundingBox(zob, scaleSurHeight, scaleSurWidth);
 
     }
     
+    private void drawZombieHealth(final Zombie zob, final int scaleSurPosX, final int scaleSurPosY, 
+                                    final int scaleSurHeight, final int scaleSurWidth){
+
+        int rectWidth = scaleSurWidth;
+        int rectHeight = 6;
+        int rectX = scaleSurPosX;
+        int rectY = scaleSurPosY - scaleSurHeight - rectHeight - 5;
+                                
+        final int maxLife = zob.getMaxZombieHealth(); 
+        int currentLife = zob.getLive();
+        int currentWidth = (int) ((double) currentLife / maxLife * rectWidth);
+                                
+        // Background bar
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect(rectX, rectY, rectWidth, rectHeight);
+                                
+        // Choose life bar color (starts red, fades to gray)
+        float healthRatio = (float) currentLife / maxLife;
+                                
+        if (healthRatio > 0.5) {
+            g2d.setColor(new Color(255, 0, 0));  // Red
+        } else if (healthRatio > 0.25) {
+            g2d.setColor(new Color(255, 140, 0));  // Orange
+        } else {
+            g2d.setColor(new Color(105, 105, 105));  // Dark Gray (almost dead)
+        }
+        g2d.fillRect(rectX, rectY, currentWidth, rectHeight);
+    }
+    
+    @SuppressWarnings("unused")
+    private void drawBoundingBox(final Zombie zob, final int scaleSurHeight, final int scaleSurWidth){
+        int scaleBboxUx = viewScale.getXinPixel(zob.getBBox().getULcorner());
+        int scaleBboxUy = viewScale.getViewHeight() - viewScale.getYinPixel(zob.getBBox().getULcorner());
+    
+        // Draw the Survivor BoundingBox
+        g2d.setColor(Color.blue);
+        g2d.drawRect(scaleBboxUx, scaleBboxUy,scaleSurWidth,scaleSurHeight);
+    }
+
 }
