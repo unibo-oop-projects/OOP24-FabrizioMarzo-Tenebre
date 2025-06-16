a80942b9cf7b1f59a66a30bd2dbfbdcca6ce73ff
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-06-16 16:17:42 +0200

    Add on input of the method drawLevel the listLevelData and the listLevelSprite

--- src/main/java/view/graphics/GraphicsLevel.java
+++ src/main/java/view/graphics/GraphicsLevel.java
@@ -1,9 +1,11 @@
 package view.graphics;
 
 import java.awt.image.BufferedImage;
+import java.util.List;
 
 import model.level.Level;
 
 public interface GraphicsLevel {
-    void drawLevel(final Level lvl, final BufferedImage img);
+    void drawLevel(final Level lvl, final List<BufferedImage> listLevelSprite, final
+    List<List<Integer>> listLevelData);
 }
