a2427f9caee33b65658e03363647cdd2cf0611e1
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-06-17 11:55:39 +0200

    Change name GameLevelType to LevelType and move this enum to model Package

--- src/main/java/model/level/LevelType.java
+++ src/main/java/model/level/LevelType.java
@@ -1,17 +1,17 @@
-package game.game_model.game_level;
+package model.level;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-public enum GameLevelType {
+public enum LevelType {
     LEVEL_TUTORIAL("LevelTutorial"),
     LEVEL_ONE("Level1"),
     BOSS("LevelBoss");
 
     private final String displayName;
 
-    GameLevelType(String displayName) {
+    LevelType(String displayName) {
         this.displayName = displayName;
     }
 
@@ -19,8 +19,8 @@ public enum GameLevelType {
         return displayName;
     }
 
-    public Optional<GameLevelType> next() {
-        List<GameLevelType> levels = Arrays.asList(GameLevelType.values());
+    public Optional<LevelType> next() {
+        List<LevelType> levels = Arrays.asList(LevelType.values());
         int nextIndex = levels.indexOf(this) + 1;
         if (nextIndex < levels.size()) {
             return Optional.of(levels.get(nextIndex));
