333a45eaa4b41c22d30a422010a7ba5cac4f982e
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-06-27 17:26:59 +0200

    Delete some comments and use the ImportImageFactory

--- src/main/java/view/graphics_util/SpriteSheetLoader.java
+++ src/main/java/view/graphics_util/SpriteSheetLoader.java
@@ -9,15 +9,6 @@ import java.awt.image.Raster;
 
 import org.apache.commons.lang3.tuple.Pair;
 
-/**
- * A utility class for loading and processing sprite animations for game entities.
- * <p>
- * This class is responsible for loading animations for survivors, zombies, and 
- * other level objects. It divides sprite sheets into frames 
- * and stores them as {@link BufferedImage} objects. It also handles loading 
- * shadows for level objects.
- * 
- */
 public class SpriteSheetLoader implements ISpriteLoader{
 
     private static final String SURVIVOR_PATH = "/sprite_sheet/survivor/";
@@ -25,29 +16,34 @@ public class SpriteSheetLoader implements ISpriteLoader{
     private static final String MUNITION_PATH = "/sprite_sheet/armory/";
     private static final String LEVEL_PATH = "/sprite_sheet/level/";
 
-
     private static final String LEVEL_OBJECT_PATH = "/level_object/";
-    private final ImportImage impImgPNG = new ImportImagePNG() ;
+    private final ImportImageFactory factory = new ImportImageFactoryImpl();
+    private final ImportImage loaderPNG = factory.loaderPNG();
 
+    @Override
     public List<List<BufferedImage>> loadSurvivorAnimations(final String nameSurvivor,final int width_frame , final int height_frame){
         return loadAnimations(SURVIVOR_PATH, nameSurvivor, width_frame, height_frame);
     }
 
+    @Override
     public List<List<BufferedImage>> loadZombieAnimations(final String nameZombie,final int width_frame , final int height_frame){
         return loadAnimations(ZOMBIE_PATH, nameZombie, width_frame, height_frame);
     }
 
+    @Override
     public List<BufferedImage> loadLevelSprite(final String nameLevelSprite,final int width_frame , final int height_frame){
         return loadSprite(LEVEL_PATH, nameLevelSprite, width_frame, height_frame);
     }
 
+    @Override
     public BufferedImage loadEntitiesShadow(final String nameObject) {
-        Optional<BufferedImage> subImage = getBBoxImage(impImgPNG.imp(LEVEL_OBJECT_PATH + nameObject));
+        Optional<BufferedImage> subImage = getBBoxImage(loaderPNG.imp(LEVEL_OBJECT_PATH + nameObject).get());
         return subImage.get();
     }
 
+    @Override
     public BufferedImage loadMunition(final String nameMunition) {
-        Optional<BufferedImage> subImage = getBBoxImage(impImgPNG.imp(MUNITION_PATH + nameMunition));
+        Optional<BufferedImage> subImage = getBBoxImage(loaderPNG.imp(MUNITION_PATH + nameMunition).get());
         return subImage.get();
     }
 
@@ -55,41 +51,36 @@ public class SpriteSheetLoader implements ISpriteLoader{
 
         int columns = img.getWidth() / width_frame;
         int rows = img.getHeight() / height_frame;
-
-        // // Check if the image is visible secondly the frame 
-        // if (img.getWidth() % width_frame != 0 || img.getHeight() % height_frame != 0) {
-        //     System.err.println("Image dimensions are not divisible by the frame size.");
-        //     return Pair.of(0, 0); // Return empty Pair 
-        // }
     
         return Pair.of(columns, rows);
     }
 
     private List<List<BufferedImage>> loadAnimations(final String pathPrefix, final String name, final int width_frame, final int height_frame) {
-        var img = impImgPNG.imp(pathPrefix + name);
+        var img = loaderPNG.imp(pathPrefix + name);
         
+
         // Check if the sprite image is load corretly 
-        if (img == null) { 
+        if (!img.isPresent()) { 
             System.err.println("Image not found: " + pathPrefix + name);
             return new ArrayList<>(); // Return the list empty of animations
         }
         
         // If the image is load corretly found his col and row 
-        var numCR = this.numColRow(width_frame, height_frame, img);
-        return this.loadSpriteSets(numCR, width_frame, height_frame, img);
+        var numCR = this.numColRow(width_frame, height_frame, img.get());
+        return this.loadSpriteSets(numCR, width_frame, height_frame, img.get());
     }
 
 
     private List<BufferedImage> loadSprite(final String pathPrefix, final String name, final int width_frame, final int height_frame){
-        var imgcity = impImgPNG.imp(pathPrefix + name);
+        var imgcity = loaderPNG.imp(pathPrefix + name);
 
-        if (imgcity == null) { 
+        if (!imgcity.isPresent()) { 
             System.err.println("Image not found: " + pathPrefix + name);
             return new ArrayList<>(); // Return the list empty of animations
         }
         
-        var numCR = this.numColRow(width_frame, height_frame, imgcity);
-        return this.loadSprites(numCR, width_frame, height_frame, imgcity);
+        var numCR = this.numColRow(width_frame, height_frame, imgcity.get());
+        return this.loadSprites(numCR, width_frame, height_frame, imgcity.get());
     }
 
 
@@ -177,15 +168,15 @@ public class SpriteSheetLoader implements ISpriteLoader{
 
     @Override
     public List<List<Integer>> getLevelData(final String nameLevelData) {
-        BufferedImage img = impImgPNG.imp(LEVEL_PATH + nameLevelData);
+        var img = loaderPNG.imp(LEVEL_PATH + nameLevelData);
         List<List<Integer>> levelData = new ArrayList<>();
     
-        Raster raster = img.getRaster();  // ottieni il raster dell'immagine
+        Raster raster = img.get().getRaster();  // ottieni il raster dell'immagine
         int numBands = raster.getNumBands(); // numero di canali (RGB, o RGBA)
     
-        for (int j = 0; j < img.getHeight(); j++) {
+        for (int j = 0; j < img.get().getHeight(); j++) {
             List<Integer> row = new ArrayList<>();
-            for (int i = 0; i < img.getWidth(); i++) {
+            for (int i = 0; i < img.get().getWidth(); i++) {
                 int[] pixel = new int[numBands];
                 raster.getPixel(i, j, pixel);  // legge tutti i canali del pixel
     
