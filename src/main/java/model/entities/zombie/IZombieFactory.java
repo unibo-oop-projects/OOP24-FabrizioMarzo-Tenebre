58a7a2b81ac73b7b6650b58b4f8b0bda4f5c56ff
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-07-03 14:15:42 +0200

    Writting the Javadoc of all class inside the survivor pakage.

--- src/main/java/model/entities/zombie/IZombieFactory.java
+++ src/main/java/model/entities/zombie/IZombieFactory.java
@@ -2,6 +2,20 @@ package model.entities.zombie;
 
 import org.apache.commons.lang3.tuple.Pair;
 
+/**
+ * Factory interface for creating {@link Zombie} instances.
+ * <p>
+ * Defines methods to instantiate different types of zombies at specified
+ * positions.
+ * </p>
+ */
 public interface IZombieFactory {
-    Zombie createClickerZombie(final Pair<Double,Double> pos);
+
+    /**
+     * Creates a new Clicker-type zombie positioned at the given coordinates.
+     *
+     * @param pos a pair (x, y) representing the initial position of the zombie
+     * @return a new {@link Zombie} instance of type Clicker
+     */
+    Zombie createClickerZombie(final Pair<Double, Double> pos);
 }
