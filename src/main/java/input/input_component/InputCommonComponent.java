3d083363ac1f6f42a29bb3ffcd544938f143293a
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-07-03 16:26:48 +0200

    Modify the class, now use tha map for coordinate actions.

--- src/main/java/input/input_component/InputCommonComponent.java
+++ src/main/java/input/input_component/InputCommonComponent.java
@@ -1,5 +1,9 @@
 package input.input_component;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
 import org.apache.commons.lang3.tuple.Pair;
 
 import utils.PairUtils;
@@ -9,68 +13,59 @@ import input.input_controller.InputController;
 import model.entities.survivor.Survivor;
 import model.entities.survivor.SurvivorState;
 
-public class InputCommonComponent implements InputSurvivorComponent{
-    
+public class InputCommonComponent implements InputSurvivorComponent {
+
     private static final Pair<Double, Double> LEFT_DIRECTION = Pair.of(-1d, 0d);
     private static final Pair<Double, Double> RIGHT_DIRECTION = Pair.of(1d, 0d);
     private static final Pair<Double, Double> UP_DIRECTION = Pair.of(0d, 1d);
     private static final Pair<Double, Double> DOWN_DIRECTION = Pair.of(0d, -1d);
-    
+
+    private final Map<Integer, Consumer<Survivor>> movementActions = new HashMap<>();
+    private final Map<Integer, Consumer<Survivor>> shootingActions = new HashMap<>();
+
+    public InputCommonComponent() {
+        movementActions.put(KeyCodes.KEY_W.getKeyCode(),
+                s -> moveSurvivor(s, UP_DIRECTION, SurvivorState.SURVIVOR_MOVE_UP));
+        movementActions.put(KeyCodes.KEY_S.getKeyCode(),
+                s -> moveSurvivor(s, DOWN_DIRECTION, SurvivorState.SURVIVOR_MOVE_DOWN));
+        movementActions.put(KeyCodes.KEY_A.getKeyCode(),
+                s -> moveSurvivor(s, LEFT_DIRECTION, SurvivorState.SURVIVOR_MOVE_LEFT));
+        movementActions.put(KeyCodes.KEY_D.getKeyCode(),
+                s -> moveSurvivor(s, RIGHT_DIRECTION, SurvivorState.SURVIVOR_MOVE_RIGHT));
+        movementActions.put(KeyCodes.NONE.getKeyCode(), s -> {
+            s.setVelocity(Pair.of(0d, 0d));
+            s.setState(SurvivorState.SURVIVOR_IDLE);
+        });
+
+        shootingActions.put(KeyCodes.ARROW_UP.getKeyCode(),
+                s -> shootInDirection(s, UP_DIRECTION, SurvivorState.SURVIOR_SHOOT_UP));
+        shootingActions.put(KeyCodes.ARROW_DOWN.getKeyCode(),
+                s -> shootInDirection(s, DOWN_DIRECTION, SurvivorState.SURVIOR_SHOOT_DOWN));
+        shootingActions.put(KeyCodes.ARROW_LEFT.getKeyCode(),
+                s -> shootInDirection(s, LEFT_DIRECTION, SurvivorState.SURVIOR_SHOOT_LEFT));
+        shootingActions.put(KeyCodes.ARROW_RIGHT.getKeyCode(),
+                s -> shootInDirection(s, RIGHT_DIRECTION, SurvivorState.SURVIOR_SHOOT_RIGHT));
+    }
+
     @Override
     public void update(final Survivor sur, final InputController ctrl) {
-
         int inputCode = ctrl.getInputCode();
 
-        if (inputCode == KeyCodes.KEY_W.getKeyCode()) {
-            CommandSurvivor.issue(sur, (s) -> {
-                s.setVelocity(PairUtils.mulScale(UP_DIRECTION, PairUtils.module(s.getBaseSurvivorVel())));
-                s.setState(SurvivorState.SURVIVOR_MOVE_UP);
-            });
-        } else if (inputCode == KeyCodes.KEY_S.getKeyCode()) {
-            CommandSurvivor.issue(sur, (s) -> {
-                s.setVelocity(PairUtils.mulScale(DOWN_DIRECTION, PairUtils.module(s.getBaseSurvivorVel())));
-                s.setState(SurvivorState.SURVIVOR_MOVE_DOWN);
-            });
-        } else if (inputCode == KeyCodes.KEY_A.getKeyCode()) {
-            CommandSurvivor.issue(sur, (s) -> {
-                s.setVelocity(PairUtils.mulScale(LEFT_DIRECTION, PairUtils.module(s.getBaseSurvivorVel())));
-                s.setState(SurvivorState.SURVIVOR_MOVE_LEFT);
-            });
-        } else if (inputCode == KeyCodes.KEY_D.getKeyCode()) {
-            CommandSurvivor.issue(sur, (s) -> {
-                s.setVelocity(PairUtils.mulScale(RIGHT_DIRECTION, PairUtils.module(s.getBaseSurvivorVel())));
-                s.setState(SurvivorState.SURVIVOR_MOVE_RIGHT);
-            });
-        } else if (inputCode == KeyCodes.NONE.getKeyCode()) {
-            CommandSurvivor.issue(sur, (s) -> {
-                s.setVelocity(Pair.of(0d, 0d));
-                s.setState(SurvivorState.SURVIVOR_IDLE);
-            });
-        } else if (inputCode == KeyCodes.ARROW_UP.getKeyCode()) {
-            CommandSurvivor.issue(sur, (s) -> {
-                s.setState(SurvivorState.SURVIOR_SHOOT_UP);
-                s.setVelocity(Pair.of(0d, 0d));
-                s.getWeapon().aim(UP_DIRECTION, s.getCurrentPos());
-            });
-        } else if (inputCode == KeyCodes.ARROW_DOWN.getKeyCode()) {
-            CommandSurvivor.issue(sur, (s) -> {
-                s.setState(SurvivorState.SURVIOR_SHOOT_DOWN);
-                s.setVelocity(Pair.of(0d, 0d));
-                s.getWeapon().aim(DOWN_DIRECTION, s.getCurrentPos());
-            });
-        } else if (inputCode == KeyCodes.ARROW_LEFT.getKeyCode()) {
-            CommandSurvivor.issue(sur, (s) -> {
-                s.setState(SurvivorState.SURVIOR_SHOOT_LEFT);
-                s.setVelocity(Pair.of(0d, 0d));
-                s.getWeapon().aim(LEFT_DIRECTION, s.getCurrentPos());
-            });
-        } else if (inputCode == KeyCodes.ARROW_RIGHT.getKeyCode()) {
-            CommandSurvivor.issue(sur, (s) -> {
-                s.setState(SurvivorState.SURVIOR_SHOOT_RIGHT);
-                s.setVelocity(Pair.of(0d, 0d));
-                s.getWeapon().aim(RIGHT_DIRECTION, s.getCurrentPos());
-            });
+        if (movementActions.containsKey(inputCode)) {
+            CommandSurvivor.issue(sur, movementActions.get(inputCode));
+        } else if (shootingActions.containsKey(inputCode)) {
+            CommandSurvivor.issue(sur, shootingActions.get(inputCode));
         }
     }
+
+    private void moveSurvivor(Survivor s, Pair<Double, Double> dir, SurvivorState state) {
+        s.setVelocity(PairUtils.mulScale(dir, PairUtils.module(s.getBaseSurvivorVel())));
+        s.setState(state);
+    }
+
+    private void shootInDirection(Survivor s, Pair<Double, Double> dir, SurvivorState state) {
+        s.setState(state);
+        s.setVelocity(Pair.of(0d, 0d));
+        s.getWeapon().aim(dir, s.getCurrentPos());
+    }
 }
-    
