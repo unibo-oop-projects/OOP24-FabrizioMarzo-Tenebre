1734f030b61c9c2078d083fdd0e6ca90acea4ea4
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-07-03 15:23:59 +0200

    Writting the javadoc of all class inside the package model.armory.charger.

--- src/main/java/model/armory/charger/Drum.java
+++ src/main/java/model/armory/charger/Drum.java
@@ -8,35 +8,62 @@ import org.apache.commons.lang3.tuple.Pair;
 import model.armory.munition.FactoryMunition;
 import model.armory.munition.Munition;
 
-public class Drum implements Charger{
+/**
+ * Implementation of the {@link Charger} interface representing a drum magazine.
+ * <p>
+ * Manages a fixed-capacity collection of ammunition,
+ * supports extracting ammunition and updating positions of contained munitions.
+ */
+public class Drum implements Charger {
 
     private List<Munition> munitions = new ArrayList<>();
     private int capacity;
-    private Pair<Double,Double> pos;
+    private Pair<Double, Double> pos;
     private FactoryMunition factory = new FactoryMunition();
 
-    public Drum(final int capacity, final Pair<Double,Double> pos) {
+    /**
+     * Constructs a new Drum charger with specified capacity and position.
+     * 
+     * @param capacity the maximum number of munitions this drum can hold
+     * @param pos      the initial position of the drum
+     */
+    public Drum(final int capacity, final Pair<Double, Double> pos) {
         this.capacity = capacity;
         this.pos = pos;
         this.fillMagazine();
     }
 
-    
-    private void fillMagazine(){
+    /**
+     * Fills the magazine to its full capacity with new munitions.
+     * Each munition is created at the current position.
+     */
+    private void fillMagazine() {
         while (this.munitions.size() < capacity) {
-            Munition newMunition = factory.createParabellum(this.pos); 
+            Munition newMunition = factory.createParabellum(this.pos);
             this.munitions.add(newMunition);
-        }        
-    }
-    
-    @Override
-    public Munition extractAmmunition() {
-        if (this.munitions.isEmpty()){
-            this.fillMagazine();
-        } 
-        return munitions.remove(munitions.size()-1);
+        }
     }
 
+    /**
+     * Extracts one unit of ammunition from the drum.
+     * If the drum is empty, it is automatically refilled before extraction.
+     * 
+     * @return the extracted {@link Munition}
+     */
+    @Override
+    public Munition extractAmmunition() {
+        if (this.munitions.isEmpty()) {
+            this.fillMagazine();
+        }
+        return munitions.remove(munitions.size() - 1);
+    }
+
+    /**
+     * Updates the position of the drum and all contained munitions
+     * that have not yet been fired.
+     * 
+     * @param pos the new position as a pair (x, y)
+     */
     @Override
     public void setPos(final Pair<Double, Double> pos) {
         this.pos = pos;
@@ -47,9 +74,14 @@ public class Drum implements Charger{
         }
     }
 
+    /**
+     * Returns the current number of ammunition units loaded in the drum.
+     * 
+     * @return the count of munitions remaining
+     */
     @Override
     public int getCurrentLoad() {
         return this.munitions.size();
     }
-    
+
 }
--- src/main/java/model/armory/charger/FactoryCharger.java
+++ src/main/java/model/armory/charger/FactoryCharger.java
@@ -2,10 +2,19 @@ package model.armory.charger;
 
 import org.apache.commons.lang3.tuple.Pair;
 
+/**
+ * Factory class for creating different types of {@link Charger} instances.
+ */
 public class FactoryCharger {
 
-    public Charger createDrumCharger(final Pair<Double,Double> pos){
-        final int capacity=5;
+    /**
+     * Creates a new {@link Drum} charger with a fixed capacity.
+     * 
+     * @param pos the initial position of the charger
+     * @return a new {@link Drum} instance with capacity 5
+     */
+    public Charger createDrumCharger(final Pair<Double, Double> pos) {
+        final int capacity = 5;
         return new Drum(capacity, pos);
     }
 }
