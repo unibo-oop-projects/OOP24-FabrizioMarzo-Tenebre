5e79053340e9c081cc5fc4bef55dd98bb563df7a
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-07-03 15:46:21 +0200

    Writting the Javadoc of all class inside the package model.physics.physics_entities.

--- src/main/java/model/physics/physics_entities/PhysicsBaseZombie.java
+++ src/main/java/model/physics/physics_entities/PhysicsBaseZombie.java
@@ -3,17 +3,27 @@ package model.physics.physics_entities;
 import model.entities.zombie.Zombie;
 import utils.PairUtils;
 
-public class PhysicsBaseZombie implements PhysicsZombieComponent{
+/**
+ * Basic implementation of the PhysicsZombieComponent interface.
+ * Updates the position of a Zombie entity based on its current velocity and
+ * elapsed time.
+ */
+public class PhysicsBaseZombie implements PhysicsZombieComponent {
 
     private static final double MILLISECONDS_TO_SECONDS = 0.001;
 
+    /**
+     * Updates the position of the given Zombie by integrating its velocity over the
+     * elapsed time.
+     *
+     * @param zob the Zombie entity to update
+     * @param dt  the elapsed time in milliseconds since the last update
+     */
     @Override
     public void updateZombie(final Zombie zob, final int dt) {
         zob.setPosition(
-            PairUtils.sum(
-                zob.getCurrentPos(),
-                PairUtils.mulScale(zob.getCurrentVel(), MILLISECONDS_TO_SECONDS * dt)
-            )
-        );
+                PairUtils.sum(
+                        zob.getCurrentPos(),
+                        PairUtils.mulScale(zob.getCurrentVel(), MILLISECONDS_TO_SECONDS * dt)));
     }
 }
--- src/main/java/model/physics/physics_entities/PhysicsSurvivorComponent.java
+++ src/main/java/model/physics/physics_entities/PhysicsSurvivorComponent.java
@@ -2,6 +2,19 @@ package model.physics.physics_entities;
 
 import model.entities.survivor.Survivor;
 
+/**
+ * Interface for components responsible for updating the physics state of a
+ * Survivor entity.
+ */
 public interface PhysicsSurvivorComponent {
+
+    /**
+     * Updates the position of the given Survivor
+     * based on the elapsed time since the last update.
+     *
+     * @param survivor        The Survivor entity to update.
+     * @param deltaTimeMillis The elapsed time in milliseconds since the last
+     *                        update.
+     */
     public void updateSurvivor(final Survivor sur, final int dt);
 }
