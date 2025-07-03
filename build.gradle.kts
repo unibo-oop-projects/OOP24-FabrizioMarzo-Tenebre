3a2308f5921ef32c9f917130392c3fcae506cd86
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-07-03 18:24:46 +0200

    modify the build.gradle.kts add task for doing test.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,21 @@
 plugins{
+    // Apply the java plugin to add support for Java
     java
+
+    // Apply the application plugin to add support for building a CLI application
+    // You can run your app via task "run": ./gradlew run
     application
     
+    /*
+     * Adds tasks to export a runnable jar.
+     * In order to create it, launch the "shadowJar" task.
+     * The runnable jar will be found in build/libs/projectname-all.jar
+     */
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.danilopianini.gradle-java-qa") version "1.108.0"
 }
 
-repositories {
+repositories {  // Where to search for dependencies
     mavenCentral()
 }
 
@@ -18,4 +28,18 @@ dependencies {
     compileOnly("com.github.spotbugs:spotbugs-annotations:4.7.3")
 
     implementation("org.apache.commons:commons-lang3:3.12.0") // Dependencies for Apache library
+
+    // JUnit API and testing engine
+    val jUnitVersion = "5.9.2"
+    // when dependencies share the same version, grouping in a val helps to keep them in sync
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events(*org.gradle.api.tasks.testing.logging.TestLogEvent.values())
+        showStandardStreams = true
+    }
 }
