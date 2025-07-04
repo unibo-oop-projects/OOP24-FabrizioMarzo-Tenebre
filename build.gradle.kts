9a7430df00d76e03fceffff98cf5aed8f2f74526
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-07-04 15:33:08 +0200

    Modify the dependencies, testImplementation mockito.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,29 @@
-plugins{
-    // Apply the java plugin to add support for Java
+plugins {
     java
-
-    // Apply the application plugin to add support for building a CLI application
-    // You can run your app via task "run": ./gradlew run
     application
-    
-    /*
-     * Adds tasks to export a runnable jar.
-     * In order to create it, launch the "shadowJar" task.
-     * The runnable jar will be found in build/libs/projectname-all.jar
-     */
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.danilopianini.gradle-java-qa") version "1.108.0"
 }
 
-repositories {  // Where to search for dependencies
+repositories {
     mavenCentral()
 }
 
 application {
-    mainClass.set("ArmataTenebre") 
+    mainClass.set("ArmataTenebre")
 }
 
 dependencies {
-    // Suppressions for SpotBugs
     compileOnly("com.github.spotbugs:spotbugs-annotations:4.7.3")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
 
-    implementation("org.apache.commons:commons-lang3:3.12.0") // Dependencies for Apache library
-
-    // JUnit API and testing engine
     val jUnitVersion = "5.9.2"
-    // when dependencies share the same version, grouping in a val helps to keep them in sync
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
 
-    testImplementation("org.mockito:mockito-core:4.+" )
-    testImplementation("org.mockito:mockito-junit-jupiter:4.+")
-
+    val mockitoVersion = "4.11.0"
+    testImplementation("org.mockito:mockito-core:$mockitoVersion")
+    testImplementation("org.mockito:mockito-junit-jupiter:$mockitoVersion")
 }
 
 tasks.test {
