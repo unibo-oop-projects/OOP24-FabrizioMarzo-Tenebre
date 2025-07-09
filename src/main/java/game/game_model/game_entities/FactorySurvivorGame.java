aa08f8e0cb8c13694996abb9dd47509b06cb24dc
Fabrizio Marzo <fabrizio.marzo@studio.unibo.it>
2025-07-09 16:56:29 +0200

    Create the Interface of the Factory of SurvivorGame

--- src/main/java/game/game_model/game_entities/FactorySurvivorGame.java
+++ src/main/java/game/game_model/game_entities/FactorySurvivorGame.java
@@ -5,26 +5,14 @@ import model.entities.survivor.Survivor;
 import view.graphics_component.survivor.GraphicsCommonComponent;
 
 /**
- * Factory class responsible for creating instances of {@link IGameSurvivor}.
- * <p>
- * Provides methods to create different types of game survivors with
- * their associated graphics and input components.
- * </p>
+ * {@inheritDoc}
  */
-public class FactorySurvivorGame {
+public class FactorySurvivorGame implements IFactorySurvivorGame {
 
     /**
-     * Creates a common type of {@link IGameSurvivor} using the provided
-     * {@link Survivor} instance.
-     * <p>
-     * This method initializes the game survivor with standard graphics and input
-     * components.
-     * </p>
-     *
-     * @param common the {@link Survivor} instance to wrap in a game survivor
-     * @return a new {@link IGameSurvivor} instance configured with common
-     *         components
+     * {@inheritDoc}
      */
+    @Override
     public IGameSurvivor gameSurvivorCommon(final Survivor common) {
         return new GameSurvivor(common,
                 new GraphicsCommonComponent(common.getClass().getSimpleName()),
---
+++ src/main/java/game/game_model/game_entities/IFactorySurvivorGame.java
@@ -0,0 +1,28 @@
+package game.game_model.game_entities;
+
+import model.entities.survivor.Survivor;
+
+/**
+ * Factory class responsible for creating instances of {@link IGameSurvivor}.
+ * <p>
+ * Provides methods to create different types of game survivors with
+ * their associated graphics and input components.
+ * </p>
+ */
+public interface IFactorySurvivorGame {
+
+    /**
+     * Creates a common type of {@link IGameSurvivor} using the provided
+     * {@link Survivor} instance.
+     * <p>
+     * This method initializes the game survivor with standard graphics and input
+     * components.
+     * </p>
+     *
+     * @param common the {@link Survivor} instance to wrap in a game survivor
+     * @return a new {@link IGameSurvivor} instance configured with common
+     *         components
+     */
+    IGameSurvivor gameSurvivorCommon(final Survivor common);
+
+}
